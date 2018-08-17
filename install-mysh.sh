d7b6d3972a22eb0dfb7dad2a2ce1bcf3e9fc924f
charlie_kickflip <c.p0yne@gmail.com>
2018-08-17 17:28:24 +0200

    added -s and -S options to timediff

--- install-mysh.sh
+++ install-mysh.sh
@@ -5,6 +5,7 @@
 ##  License: see LICENSE (GNU GPL v3)
 ## DESCRIPTION
 ##  this will install all the shell scripts in this directory to $HOME/bin (by default) or the directory provided to ${1}
+##  an installation is only necessary when new scripts have been added, and/or script names have changed, etc
 ## PARAMETERS
 ##  1) instdir=${1}    # the desired installation directory
 erro(){ echo "$@" 1>&2; }
--- timediff.sh
+++ timediff.sh
@@ -4,19 +4,27 @@
 ##  Copyright (C): 2018
 ##  License: see LICENSE (GNU GPL v3)
 ## DESCRIPTION
-##  this script takes in two times (in the format DDD:HH:MM:SS) and outputs their absolute difference (in the format HH:MM:SS)
+##  this script takes in two times (in the format DDD:HH:MM:SS) and outputs their absolute difference (in the format HH:MM:SS and/or in total seconds)
 ##  to get the current time in the format DDD:HH:MM:SS one can execute: date +%j:%H:%M:%S (my alias is `rawdate`)
 ##  in pseudo-formula: HH:MM:SS = abs(DDDF:HHF:MMF:SSF - DDDI:HHI:MMI:SSI)
 ##  it has the restrictions: 001 <= DDD <= 366, 00 <= HH =< 23, 00 <= MM <= 59, 00 <= SS <= 59
 ##  NOTE: it will break for calculating differences that cross over New Year's...
+## OPTIONS
+##  -u for "usage": see script usage
+##  -h for "help": less the relevant documentation and see script usage
+##  -s for "seconds": also output the difference in total seconds
+##  -S for "seconds only": output the difference in total seconds only
 ## PARAMETERS
 ##  1) timeI=${1}    # the initial (I) time, in the format DDD:HH:MM:SS
 ##  2) timeF=${2}    # the final (F) time, in the format DDD:HH:MM:SS
 erro(){ echo "$@" 1>&2; }
-myUsage(){ erro "Usage${1}: `basename ${0}` [-u for usage] <DDD:HH:MM:SS> <DDD:HH:MM:SS>"; exit 1; }
+myUsage(){ erro "Usage${1}: `basename ${0}` [-u for usage] [-h for help] [-s for seconds] [-S for seconds only] <DDD:HH:MM:SS> <DDD:HH:MM:SS>"; exit 1; }
 timeI=${1}    # the initial (I) time, in the format DDD:HH:MM:SS
 timeF=${2}    # the final (F) time, in the format DDD:HH:MM:SS
-somevalue='<insert value>'    # <insert description, this is used below>
+mysh=$MYSH    # this must point to where this current script lives
+szero='0'
+sone='1'
+stwo='2'
 
 
 
@@ -52,21 +60,40 @@ rezero(){
 }
 
 
-# parse the input
+# pre-parse the script parameters
 if [ -z ${1} ]
 then
   erro 'ERROR 0: god is empty, just like me...'
   exit 1
-elif [ ${1} = '-u' ]
-then
-  myUsage
-elif [ ${1:0:1} = '-' ]
-then
-  myUsage " (option -${1:1} not recognized)"
-elif [ ${#} -ne 2 ] # check that the right number of script paramters have been filled
+fi
+Sswitch=$szero # this gets turned on by the -s or -S option
+while getopts ":uhsS" myopt # filter the script options
+do
+  case "${myopt}" in
+    u) # -u for "usage": see script usage
+      myUsage;;
+    h) # -h for "help": less the relevant documentation and see script usage
+      sed -n '2,22p; 23q' $mysh/timediff.sh | command less
+      myUsage
+      ;;
+    s) # -s for "seconds": also output the difference in total seconds
+      Sswitch=$sone;;
+    S) # -S for "seconds only": output the difference in total seconds only
+      Sswitch=$stwo;;
+    \?)
+      myUsage " (option -${OPTARG} not recognized)";;
+  esac
+done
+shift $(($OPTIND-1))
+if [ ${#} -ne 2 ] # check that the right number of script paramters have been filled
 then
   myUsage ' (incorrect number of script parameters)'
 fi
+timeI=${1}    # the initial (I) time, in the format DDD:HH:MM:SS
+timeF=${2}    # the final (F) time, in the format DDD:HH:MM:SS
+
+
+# parse the input
 if [[ $timeI != ???:??:??:?? ]] || [[ $timeF != ???:??:??:?? ]]
 then
   myUsage ' (incorrect format of script parameters)'
@@ -103,58 +130,65 @@ fi
 
 
 # pre-check
+if [ $Sswitch != $szero ] && [ $Sswitch != $sone ] && [ $Sswitch != $stwo ]
+then
+  erro 'ERROR 1207: Sswitch is out of bounds!?'
+  erro "Sswitch = $Sswitch"
+  erro 'exiting...'
+  exit 1
+fi
 if [ $DDDI -lt 1 ] || [ 366 -lt $DDDI ]
 then
-  erro 'ERROR: timeI is out of range!'
+  erro 'ERROR 0508: timeI is out of range!'
   erro "DDDI = $DDDI"
   erro 'exiting...'
   exit 1
 fi
 if [ $HHI -lt 0 ] || [ 23 -lt $HHI ]
 then
-  erro 'ERROR: timeI is out of range!'
+  erro 'ERROR 1135: timeI is out of range!'
   erro "HHI = $HHI"
   erro 'exiting...'
   exit 1
 fi
 if [ $MMI -lt 0 ] || [ 59 -lt $MMI ]
 then
-  erro 'ERROR: timeI is out of range!'
+  erro 'ERROR 9056: timeI is out of range!'
   erro "MMI = $MMI"
   erro 'exiting...'
   exit 1
 fi
 if [ $SSI -lt 0 ] || [ 59 -lt $SSI ]
 then
-  erro 'ERROR: timeI is out of range!'
+  erro 'ERROR 0071: timeI is out of range!'
   erro "SSI = $SSI"
   erro 'exiting...'
   exit 1
 fi
 if [ $DDDF -lt 1 ] || [ 366 -lt $DDDF ]
 then
-  erro 'ERROR: timeF is out of range!'
+  erro 'ERROR 2863: timeF is out of range!'
   erro "DDDF = $DDDF"
   erro 'exiting...'
   exit 1
 fi
 if [ $HHF -lt 0 ] || [ 23 -lt $HHF ]
 then
-  erro 'ERROR: timeF is out of range!'
+  erro 'ERROR 1285: timeF is out of range!'
   erro "HHF= $HHF"
   erro 'exiting...'
   exit 1
 fi
 if [ $MMF -lt 0 ] || [ 59 -lt $MMF ]
 then
-  erro 'ERROR: timeFis out of range!'
+  erro 'ERROR 3104: timeFis out of range!'
   erro "MMF = $MMF"
   erro 'exiting...'
   exit 1
 fi
 if [ $SSF -lt 0 ] || [ 59 -lt $SSF ]
 then
-  erro 'ERROR: timeF is out of range!'
+  erro 'ERROR 5771: timeF is out of range!'
   erro "SSF = $SSF"
   erro 'exiting...'
   exit 1
@@ -164,16 +198,29 @@ fi
 
 # perform the calculation
 DIFFS=$(($TOTSF-$TOTSI)) # total difference in seconds
-DHH=$(($DIFFS/3600)) # calculate the HH (recall that $((...)) automatically floors to an integer in bash)
-deltatmp=$(($DIFFS - 3600*$DHH))
-DMM=$(($deltatmp/60))
-DSS=$(($deltatmp - 60*DMM))
+if [ $Sswitch = $szero ] || [ $Sswitch = $sone ]
+then
+  DHH=$(($DIFFS/3600)) # calculate the HH (recall that $((...)) automatically floors to an integer in bash)
+  deltatmp=$(($DIFFS - 3600*$DHH))
+  DMM=$(($deltatmp/60))
+  DSS=$(($deltatmp - 60*DMM))
+
+  DHH=$(rezero $DHH)
+  DMM=$(rezero $DMM)
+  DSS=$(rezero $DSS)
+fi
 
 # output in the final format
-DHH=$(rezero $DHH)
-DMM=$(rezero $DMM)
-DSS=$(rezero $DSS)
-echo "${DHH}:${DMM}:${DSS}" # return the difference in the format HH:MM:SS
+if [ $Sswitch = $szero ]
+then
+  echo "${DHH}:${DMM}:${DSS}" # return the difference in the format HH:MM:SS
+elif [ $Sswitch = $sone ]
+then
+  echo "${DHH}:${DMM}:${DSS} = ${DIFFS} seconds" # return the difference in the format HH:MM:SS and in total seconds
+elif [ $Sswitch = $stwo ]
+then
+  echo "${DIFFS}" # return the difference in total seconds only
+fi
 
 
 
